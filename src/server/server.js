4d6884d515f37fc0d2608414ed75c922a644ccd9
Jeckel <jeckel@users.noreply.github.com>
2017-11-21 10:57:35 +0100

    Remove weather

--- src/server/server.js
+++ src/server/server.js
@@ -2,15 +2,13 @@ import mqtt from 'mqtt';
 import settings from 'settings';
 import Logger from 'server/logger';
 import Listener from 'server/listener';
-import Weather from 'server/weather';
 
 const logger = new Logger();
 
 logger.info('Starting server...');
 
-const mqttClient = mqtt.connect(`mqtt://${settings.mosca.host}`);
+const mqttClient = mqtt.connect(`mqtt://${settings.mqtt.host}`);
 const listener = new Listener({ settings, logger });
-const weather = new Weather({ logger, settings });
 
 mqttClient.on('connect', () => {
   logger.info('MQTT Server connected');
@@ -20,16 +18,3 @@ mqttClient.on('connect', () => {
 mqttClient.on('message', (topic, message) => listener.handle(topic, message));
 
 logger.info('Server is running.');
-
-// Loading current weather and publish
-setInterval(() => {
-  weather.find((err, res) => {
-    if (err) {
-      logger.error(err.message, { err });
-      return;
-    }
-    logger.info(`Publish current temperature: ${res.temperature}`);
-    mqttClient.publish('sensor/weather/temperature', JSON.stringify({ value: res.temperature }));
-
-  })
-}, settings.weather.interval);
--- src/settings.js
+++ src/settings.js
@@ -2,19 +2,15 @@
  * Settings
  */
 module.exports = {
-  'mosca': {
+  mqtt: {
     host: process.env.MQTT_HOST || 'mqtt_server',
     port: process.env.MQTT_POST || 1883
   },
-  'postgres': {
+  postgres: {
     user: process.env.POSTGRES_USER || 'admin',
     host: process.env.POSTGRES_HOST || 'storage',
     database: process.env.POSTGRES_DATABASE || 'omeglast',
     password: process.env.POSTGRES_PASSWORD || 'admin',
     port: process.env.POSTGRES_PORT || 5432
-  },
-  'weather': {
-    location: 'Paris, FR',
-    interval: 5000
   }
 };
