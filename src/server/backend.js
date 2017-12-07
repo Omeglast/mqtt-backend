1d85ee016fe1186882eb64b0ca56c1216b7cb491
Julien MERCIER <devci@j3ck3l.me>
2017-12-07 22:13:51 +0100

    Work in progress

--- src/server/backend.js
+++ src/server/backend.js
@@ -4,7 +4,28 @@ class Backend {
     this.logger = logger;
   }
 
-  saveValue(time, sensor, value, unit = null) {
+  saveTs(sensor, value, time) {
+    this.postgres.query(
+      'INSERT INTO ts(time, sensor, value) VALUES($1, $2, $3) RETURNING *',
+      [ time, sensor, value ],
+      (err, res) => {
+        if (err) {
+          this.logger.error(err.message, { err });
+          return;
+        }
+        this.logger.debug('Record saved', { res });
+      });
+  }
 
+  insertOrUpdatePeriod(sensor, value, time, unit) {
+    this.postgres.query("SELECT value FROM ts_periods WHERE sensor = $1 ORDER BY start_tz DESC LIMIT 1", [ sensor ])
+      .then(found => )
+  }
+
+  saveValue(sensor, value, time = new Date(), unit = null) {
+    this.saveTs(sensor, value, time);
+    this.insertOrUpdatePeriod(sensor, value, time, unit);
   }
 }
+
+export default Backend;
