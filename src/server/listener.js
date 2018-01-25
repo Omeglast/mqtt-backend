706867e72c939b017ec3841d5ee1b4a71c649fe2
Julien MERCIER <devci@j3ck3l.me>
2018-01-26 00:17:43 +0100

    Working version with InfluxDB

--- src/server/listener.js
+++ src/server/listener.js
@@ -1,3 +1,22 @@
+const Influx = require('influx');
+
+const influx = new Influx.InfluxDB({
+    host: 'mqtt-influxdb',
+    database: 'omeglast',
+    schema: [
+        {
+            measurement: 'sensors',
+            fields: {
+//                sensor: Influx.FieldType.STRING,
+                value: Influx.FieldType.INTEGER
+            },
+            tags: [
+                'sensor'
+            ]
+        }
+    ]
+});
+
 class Listener {
   constructor({ postgres, logger }) {
     this.postgres = postgres;
@@ -19,6 +38,26 @@ class Listener {
       }
       this.logger.debug('Record saved', { res });
     });
+
+    influx.writePoints([
+        {
+            measurement: 'sensors',
+            tags: { sensor },
+            fields: { value },
+        }
+    ]).then(() => {
+          return influx.query(`
+    select * from response_times
+    where host = ${Influx.escape.stringLit('backend')}
+    order by time desc
+    limit 10
+  `)
+      }).then(rows => {
+          rows.forEach(row => console.log(`A request to ${row.path} took ${row.duration}ms`))
+      })
+
+
+
   }
 
   handle(topic, message) {
